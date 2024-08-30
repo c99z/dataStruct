29256a60c9e9e62496f28cbbdef0a03ce5d5fe6a
llz <buptczcz@163.com>
2024-08-30 20:45:20 +0800

    8.30

--- 01 链表.cpp
+++ 01 链表.cpp
@@ -12,6 +12,7 @@ public:
     Node<T>() //默认构造函数，创建的新节点指针域指向空
     {
         this->next = nullptr;
+
     };
     Node<T>(T data)
     {
@@ -97,8 +98,9 @@ template<class T>
 
 
         }
-        friend T Reverse2(const Clink<T>& link);
-    private:
+        friend void Reverse2(const Clink<T>& link);
+        //frined void ReverseLink(const Node<T>& head;
+    public:
         Node<T>* head;//创建头节点
         
     };
@@ -131,28 +133,68 @@ template<class T>
 
     }
     //链表逆序
-template<class T>
-T Reverse2(const Clink<T> &link)//引用方式传入一个链表
+//template<class T>
+void Reverse2(Clink<int> &link)//常量引用方式传入一个链表,实际上只需要一个头节点
 {
-    Node<T>* p = link<T>.head->next;
-    Node<T>* q = nullptr;
-    link<T>.head->next= nullptr;//转换后的首节点变成尾节点，所以其地址域要设为空
+    Node<int>* p = link.head->next;
+    Node<int>* q = nullptr;
+    link.head->next= nullptr;//转换后的首节点变成尾节点，所以其地址域要设为空
 
     while (p!=nullptr)
     {
         q = p->next;//保存下一个节点
-        p->next = link<T>.head->next;
-        link<T>.head->next = p;
+        p->next = link.head->next;
+        link.head->next = p;
         p = q;
         q = nullptr;
     }
-    
+}
+//函数模板逆序
+/*不一定非要传入整个单链，实际上传入头节点就行*/
+template<class T>
+void ReverseLink(Node<T>& head)
+{
+    Node<T>* p =head.next;
+    Node<T>* q = nullptr;
+    head.next = nullptr;//转换后的首节点变成尾节点，所以其地址域要设为空
 
+    while (p != nullptr)
+    {
+        q = p->next;//保存下一个节点
+        p->next =head.next;
+        head.next = p;
+        p = q;
+        q = nullptr;
+    }
+}
+//求单链表倒数第k个节点
+/*z这种方法，如果链表很长，但是k也很大，不如正数快*/
+template <class T>
+void ReverseCount(Node<T>& head)
+{
+    int k=0;
+    int n = 0;//节点个数
+    Node<T>* q = head.next;
+    while (q!=nullptr)
+    {
+        n = n+1;
+        q = q->next;
+    }
 
+    cout << "总个数为：" << n << endl;
+    cout << "请输入你要查找倒数第几个节点：" << endl;
+    cin >> k;
+    Node<T>* p = head.next;
+    for (int i = 1; i < n-k+1; i++)
+    {
+        p = p->next;
+    }
+    cout << p->data << endl;
 }
 
 void test03()
-{
+{   
+
     Clink<int> link;
     link.InsertFirst(10);
     link.InsertFirst(520);
@@ -160,17 +202,46 @@ void test03()
     link.InsertFirst(220);
     link.InsertFirst(230);
     link.Show();
-    Reverse2<int>(link);
+    Reverse2(link);
+    link.Show();
+    ReverseLink(*link.head);
     link.Show();
 
 }
+ 
+void test04()
+{
+    Clink<string> link2;
+    link2.InsertFirst("张三");
+    link2.InsertFirst("萨三");
+    link2.InsertFirst("黎三");
+    link2.InsertFirst("五三");
+    link2.Show();
+    ReverseLink(*link2.head);
+    link2.Show();
 
+}
+
+void test05()
+{
+    Clink<string> link2;
+    link2.InsertFirst("张三");
+    link2.InsertFirst("萨三");
+    link2.InsertFirst("黎三");
+    link2.InsertFirst("五三");
+    link2.Show();
+    ReverseCount(*link2.head);
+
+
+}
 int main()
 {
+    
     //test01();
     //test02();
     test03();
-
+    //test04();
+    //test05();
     system("pause");
     return 0;
 }
