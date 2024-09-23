af4e2dc1f99c7bff3b2e8cdf4872eae9c38b5ca9
卓 陈 <1276939274@qq.com>
2024-09-23 16:12:16 +0800

    9.23

---
+++ 08 BST树.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class BSTree
+{
+public:
+	BSTree()
+	{
+		Node* root_ = nullptr;
+	}
+	~BSTree()
+	{
+		delete* root;
+	}
+
+	void Insert(const T &val)
+	{
+		if (root_==nullptr)//没有根节点，生成一个
+		{
+			Node* cur = new Node(val);
+			//cur->data_ = val;
+			root_= cur;
+			delete cur;
+		}
+		/*有跟节点 生成新的节点然后 比较大小*/
+		Node* cul = new Node(val);
+		Node* p = root_;
+		while (p != nullptr)//遍历到叶子节点为止
+		{
+			if (val >= p->data_)
+			{
+				p = p->right_;
+				if (val < p->left_)
+				{
+
+				}
+			}
+			else
+			{
+				p = p->left_;  
+				if (val >= p->right_)
+				{
+					break;
+				}
+			}
+		}
+	}
+	bool empty()
+	{
+		return root_;
+	}
+	struct Node
+	{
+		Node(T data = T())
+		:left_(nullptr)
+		,right_(nullptr)
+		,data_=data
+	{}
+		T data_;
+		Node* left_;
+		Node* right_;
+	};
+private:
+	Node* root_;//指向根节点的指针 初始化的时候指向空
+};
+
+void test01()
+{
+	BSTree<int> s1;
+
+
+}
+
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
---
+++ 08.2 int型bst树.cpp
@@ -0,0 +1,26 @@
+class intBSTree
+{
+public:
+	intBSTree()
+	{
+
+	}
+	~intBSTree();
+
+	struct Node
+	{
+		int data_;
+		int* left_;
+		int* right_;
+	};
+private:
+	
+};
+
+intBSTree::intBSTree()
+{
+}
+
+intBSTree::~intBSTree()
+{
+}
